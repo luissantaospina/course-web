6e599c27b63e0f528ae618044bcda42670bff617
Luis <luisfernandosantaospina@gmail.com>
2023-04-28 15:43:18 -0500

    Add sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseComponent } from './components/course/course.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http';
+import { CourseCreateComponent } from './components/course/course-create/course-create.component';
+import { CourseUpdateComponent } from './components/course/course-update/course-update.component';
+import { CourseDeleteComponent } from './components/course/course-delete/course-delete.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseComponent
+    CourseComponent,
+    CourseCreateComponent,
+    CourseUpdateComponent,
+    CourseDeleteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
