517975fb48c17ba4a6a1861e62c3f008462a88d8
Luis <luisfernandosantaospina@gmail.com>
2023-04-28 16:17:37 -0500

    Add create course

--- src/app/components/course/course-create/course-create.component.spec.ts
+++ src/app/components/course/course-create/course-create.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CourseComponent } from './course.component';
+import { CourseCreateComponent } from './course-create.component';
 
-describe('CourseComponent', () => {
-  let component: CourseComponent;
-  let fixture: ComponentFixture<CourseComponent>;
+describe('CourseCreateComponent', () => {
+  let component: CourseCreateComponent;
+  let fixture: ComponentFixture<CourseCreateComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CourseComponent ]
+      declarations: [ CourseCreateComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(CourseComponent);
+    fixture = TestBed.createComponent(CourseCreateComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
